e5c7a37ba2342d9ce9cf6dc081fa59602bf4d385
Bora Saglam <posarobit@gmail.com>
2025-06-16 11:41:16 +0300

    Odevler.py değişiklik yaptım.

--- Odevler.py
+++ Odevler.py
@@ -1,6 +1,6 @@
-
-Ad = input("Lütfen adınızı giriniz: ")
-       
+import keyboard
+for name in range(3):
+    Ad = input("Lütfen adınızı giriniz: ")      
 if not Ad.strip().isalpha():
     print("Adınızı girmek için kelime girmelisiniz.")
     print("Ad bilgisini bos gecemezsiniz !")
@@ -14,13 +14,13 @@ if not Soyad.isalpha():
     print(" Programı tekrar çalıştırıp doğru bir yaş değeri girmelisiniz.")       
 yas = input("Lütfen yaşınızı giriniz: ")
 if not yas.strip().isdigit():
-        print("Yaş bilgisi için sayı girmelisiniz.") 
-        print(" Yas bilgisini bos gecemezsiniz !")
+    print("Yaş bilgisi için sayı girmelisiniz.") 
+    print(" Yas bilgisini bos gecemezsiniz !")
 
-#if (yas.strip())== "":
-    #print("Programı tekrar çalıştırıp doğru bir yaş değeri girmelisiniz."
+if (yas.strip())== "":
+    print("Programı tekrar çalıştırıp doğru bir yaş değeri girmelisiniz.")
 if int(yas) >=0 and int(yas)<= 12:
-        print("Yaşınız:" , yas, "\n -> Çocuk Kategorisinidesiniz.")
+    print("Yaşınız:" , yas, "\n -> Çocuk Kategorisinidesiniz.")
 if int(yas) >= 13 and int(yas) < 17:
         print("Yaşınız:" , yas, "\n -> Genç Kategorisindesiniz.")
 if int(yas) >= 18 and int(yas) <= 24:
@@ -29,3 +29,10 @@ if int(yas) >= 25:
         print("Yaşınız:" , yas, "\n -> Yetişkin Kategorisindesiniz.")  
         kategori = input(" 0-12 yaş -> Çocuk kategorisi\n, 13-17 yaş -> Genç Kategorisi\n, 18-24 yaş Genç Yetişkin\n, =>25 -> Yetişkin Kategorisi")   
         print("Hoşgeldiniz", Ad, Soyad, yas, kategori)
+
+sayi2 = (2 ** 3) ** 2  
+print("İşlem Sonucu:", sayi2)
+
+sayi3 = (5 ** 2 + 5) * 2 / 7
+print("İşlem Sonucu:",sayi3)
+print(round(sayi3))
